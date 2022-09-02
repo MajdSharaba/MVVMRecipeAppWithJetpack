69748c60c0720db8898bcf025083ecdb4c555c44
bob <MajdSharabah8396@gmail.com>
2022-09-02 14:37:34 +0300

    init commit

---
+++ app/src/main/java/com/codingwithmitch/mvvmrecipeapp/presentation/ui/recipe_list/RecipeListViewModel.kt
@@ -0,0 +1,36 @@
+package com.codingwithmitch.mvvmrecipeapp.presentation.ui.recipe_list
+
+import android.util.Log
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.codingwithmitch.mvvmrecipeapp.domain.model.Recipe
+import com.codingwithmitch.mvvmrecipeapp.repository.RecipeRepository
+import com.codingwithmitch.mvvmrecipeapp.util.TAG
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+import javax.inject.Named
+
+const val PAGE_SIZE = 30
+
+const val STATE_KEY_PAGE = "recipe.state.page.key"
+const val STATE_KEY_QUERY = "recipe.state.query.key"
+const val STATE_KEY_LIST_POSITION = "recipe.state.query.list_position"
+const val STATE_KEY_SELECTED_CATEGORY = "recipe.state.query.selected_category"
+
+@HiltViewModel
+class RecipeListViewModel
+@Inject
+constructor(
+    private val repository: RecipeRepository,
+    private @Named("auth_token") val token: String,
+): ViewModel(){
+
+    fun getRepo() = repository
+
+    fun getAuthToken() = token
+}
